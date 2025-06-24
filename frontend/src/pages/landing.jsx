5b61edef41a6ae40ab6ad830a50fc894540790f5
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-06-24 15:12:10 +0530

    update landing page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -7,33 +7,47 @@ export default function LandingPage() {
     const router = useNavigate();
 
     return (
-        <div className="landingPageContainer">
+        <div className='landingPageContainer'>
             <nav>
-                <div className="navHeader">
-                    <h2>STREAMMATE</h2>
+                <div className='navHeader'>
+                    <h2>Apna Video Call</h2>
                 </div>
-                <div className="navlist">
-                    <p onClick={() => router("/aljk23")}>Guest</p>
-                    <p onClick={() => router("/auth")}>Register</p>
-                    <div onClick={() => router("/auth")} role="button">
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
                         <p>Login</p>
                     </div>
                 </div>
             </nav>
+
+
             <div className="landingMainContainer">
                 <div>
-                    <h1>
-                        <span style={{ color: "#FF9839" }}>Connect</span> instantly
-                    </h1>
-                    <p>Video call, message, and share with one click. No downloads.</p>
-                    <div role="button">
-                        <Link to="/auth">Get Started</Link>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by Apna Video Call</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
                     </div>
                 </div>
                 <div>
-                    <img src="/mobile.png" alt="StreamMate app" />
+
+                    <img src="/mobile.png" alt="" />
+
                 </div>
             </div>
+
+
+
         </div>
     )
 }
