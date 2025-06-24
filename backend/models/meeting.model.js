5df70c324637716242aaf21ece792c27268b8ca9
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-06-24 15:04:44 +0530

    update the meeting model file

--- backend/models/meeting.model.js
+++ backend/models/meeting.model.js
@@ -5,9 +5,7 @@ const meetingSchema = new Schema({
     meetingCode: {type: String, required: true},
     meetingName: {type: String, required: true},
     date: {type: Date, required: true , default: Date.now},
-    startTime: {type: String, required: true},
-    endTime: {type: String, required: true},
-    meetingLink: {type: String, required: true},
+    
 })
 
 const Meeting = mongoose.model("Meeting", meetingSchema);
