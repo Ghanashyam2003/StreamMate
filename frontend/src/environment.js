a5159f7dcf0e8a4f476ab8ab40ad33aa37e2bb7a
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-06-24 16:25:06 +0530

    The project is live on render

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,8 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://streammate-ue0p.onrender.com" :
 
-    "http://localhost:8000"
+    "http://localhost:3000"
 
 
 export default server;
