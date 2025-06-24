e8ba47175e9334ca8d9887ffa1423af8cecaff6a
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-06-24 15:10:18 +0530

    add the history componet

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,16 +1,33 @@
-import { Router } from 'express';
-import { login, register } from '../controllers/user.controller.js';
+const getUserHistory = async (req, res) => {
+    const { token } = req.query;
 
-const router = Router();
+    try {
+        const user = await User.findOne({ token: token });
+        const meetings = await Meeting.find({ user_id: user.username })
+        res.json(meetings)
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
+    }
+}
 
-router.post("/login", login);
-router.post("/register", register);
-router.post("/add_to_attivity", (req, res) => {
-  // Add your handler here
-});
-router.get("/get_all_activities", (req, res) => {
-  // Add your handler here
-});
+const addToHistory = async (req, res) => {
+    const { token, meeting_code } = req.body;
 
-export default router;
+    try {
+        const user = await User.findOne({ token: token });
 
+        const newMeeting = new Meeting({
+            user_id: user.username,
+            meetingCode: meeting_code
+        })
+
+        await newMeeting.save();
+
+        res.status(httpStatus.CREATED).json({ message: "Added code to history" })
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
+    }
+}
+
+
+export { login, register, getUserHistory, addToHistory }
